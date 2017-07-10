11db0994a803abfe1fe3edef3bdfd8b10ce97153
Brendan Samek <samek@ualberta.ca>
2017-07-10 01:12:48 -0600

    fix sql

--- importer.py
+++ importer.py
@@ -96,8 +96,8 @@ def update_pricing(cursor, cost, price):
 
 def add_pricing(cursor, cost, price):
     cursor.execute("""INSERT INTO inventory_pricing 
-        (inventory_id, unit_cost, unit_sell) 
-        VALUES (LAST_INSERT_ID(), %s, %s) """,
+        (inventory_id, unit_cost, unit_sell, store_id) 
+        VALUES (LAST_INSERT_ID(), %s, %s, 1) """,
         (cost, price))
 
 if __name__ == "__main__":
@@ -111,4 +111,6 @@ if __name__ == "__main__":
         if filename == "naxos.txt":
             for row in tsvin:
                 import_naxos(c, row)
+
+add_to_db(c, "TEST", 123456, 2, "123L", "BaCH", "PATH", 2017, 310151, 23, 12.99, 21.99)
 
