c4adbfe142f227b150dbec1f7cc22e23719d5d4e
Brendan Samek <samek@ualberta.ca>
2017-07-09 23:02:30 -0600

    Finish import naxos and dependencies

--- import.py
+++ import.py
@@ -1,9 +1,5 @@
 import MySQLdb
 
-db = MySQLdb.connect("localhost", "root", "76trombones", "weberp")
-c = db.cursor()
-
-
 def check_upc(cursor, upc):
     cursor.execute("""SELECT upc from inventory WHERE upc = %s""", upc)
     return cursor.rowcount
@@ -16,30 +12,80 @@ def get_medium_id(cursor, medium):
     
     return cursor.fetchone()[0]
 
-def get_label_id(cursor, label, distributor):
-    pass
-
-def get_distributor_id(cursor, distributor):
-    cursor.execute("""SELECT distributor_id from medium where distributor_name = %s""", medium)
+def get_label_id(cursor, label, distributor_id):
+    cursor.execute("""SELECT label_id from label where distributor_name = %s""", medium)
     if not cursor.countrows:
         raise ValueError(f"Could not find distributor: {distributor} in database")
-    
-    
-    return cursor.fetchone()[0]
+    else:
+        return cursor.fetchone()[0]
 
-def import_naxos():
+def get_distributor_id(cursor, distributor):
+    cursor.execute("""SELECT distributor_id from distributor where distributor_name = %s""", distributor)
+    if not cursor.countrows:
+        raise ValueError(f"Could not find distributor: {distributor} in database")
+    else:
+        return cursor.fetchone()[0]
+
+def add_to_db(cursor, title, upc, medium_id, cd_number, composer, artist, year, label, distributor):
+    cursor.execute("""INSERT INTO inventory 
+                   (cd_title,
+                    medium_id,
+                    distributor_cd_number,
+                    upc,
+                    artist,
+                    composer,
+                    catalogue_listing_year,
+                    entered_on, 
+                    updated)
+                    VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, NOW(), NOW()""",
+                    (title, medium_id, cd_number, upc, artist, composer, year, label_id, distributor_id))
+    if not cursor.rowcount:
+        print(f"failed insert on upc {upc}")
+
+def update_db(cursor, title, upc, medium_id, cd_number, composer, artist, year, label, distributor):
+    cursor.execute("""UPDATE inventory SET
+                cd_title = %s,
+                medium_id = %s,
+                distributor_cd_number = %s
+                artist = %s,
+                composer = %s,
+                catalogue_listing_year = %s,
+                label_id = %s,
+                distributor_id = %s
+                updated = NOW()
+                WHERE upc = %s""", 
+                (title, medium_id, cd_number, artist, composer, year, label_id, distributor_id, upc))
+    
+    if not cursor.rowcount:
+        print(f"failed update on upc {upc}")
+
+def import_naxos(cursor, row):
     title, upc, medium, cd_number, composer, artist, year, label = row
+    distributor = "Naxos"
 
+    distributor_id = get_distributor_id(cursor, distributor)
+    label_id = get_label_id(cursor, label, distributor_id)
+    medium_id = get_medium_id(cursor, medium)
+
+    if check_upc(cursor, upc):
+        update_db(cursor, title, upc, medium_id, cd_number, composer, artist, year, label, distributor)
+    else:
+        add_to_db(cursor, title, upc, medium_id, cd_number, composer, artist, year, label, distributor)
 
 
 
 if __name__ == "__main__":
     import csv
     from sys import argv
+    
+
+    db = MySQLdb.connect("localhost", "root", "76trombones", "weberp")
+    c = db.cursor()
+
     filename = argv[1]
     with open(argv[1], 'r', encoding='latin_1') as tsvfile:
         tsvin = csv.reader(tsvfile, delimiter='\t')
 
-        if filename = "naxos.txt":
+        if filename == "naxos.txt":
             for row in tsvin:
-                import_naxos(db, row)
+                import_naxos(c, row)
