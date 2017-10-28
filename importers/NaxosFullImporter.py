a93495c11da8ef26e8e33e3f6c3080ea3a97b714
Brendan Samek <xrendan@gmail.com>
2017-10-28 14:50:00 -0600

    Change naxos full importer for canadian format

--- importers/NaxosFullImporter.py
+++ importers/NaxosFullImporter.py
@@ -8,16 +8,20 @@ class NaxosFullImporter(GenericImporter):
     def execute_row(self, row):
         super().execute_row(row)
         _, label, _, cd_number, title, upc, _, _, year, artist, composer, medium, _ = row
-        cost = 0
+        cd_number, upc, composer, title, artist, _, cost, _, label, year, medium, _ = row
+
         distributor = "Naxos"
 
         distributor_id = self.get_distributor_id(distributor)
         label_id = self.get_label_id(label, distributor_id)
         medium_id = self.get_medium_id(medium)
-        price = 0
+        price = self.get_sales_price(cost)
 
         if self.check_upc(upc):
-            pass
+            self.update_row(title, upc, medium_id, cd_number, composer, artist, year, label_id, distributor_id, cost,
+                      price)
+            print("update")
         else:
-            self.add_row(title, upc, medium_id, cd_number, composer, artist, year, label_id, distributor_id, cost, price)
+            self.add_row(title, upc, medium_id, cd_number, composer, artist, year, label_id, distributor_id, cost,
+                         price)
             print("add")
