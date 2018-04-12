2e1b4081582542eacc072cb7a014cc5f28acd516
Brendan Samek <xrendan@gmail.com>
2018-04-11 19:48:22 -0600

    Add support for Naxos Deletion

--- importers/NaxosDeleter.py
+++ importers/NaxosDeleter.py
@@ -5,3 +5,14 @@ class NaxosDeleter(GenericImporter):
     def __init__(self, db):
         super().__init__(db)
 
+    def execute_row(self, row):
+        super().execute_row(row)
+        _, _, _, upc, *_ = row
+
+        if self.check_upc(upc):
+            self.discontinue(upc)
+            print("deleted")
+        else:
+            print("Item to delete not in database")
+
+
