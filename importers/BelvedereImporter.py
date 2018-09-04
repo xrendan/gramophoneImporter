cff254a7c1cee6ba0ff75b74d26d056c5d1aebaf
Brendan Samek <xrendan@gmail.com>
2018-09-04 11:38:39 -0600

    Add Belvedere to Naxos

---
+++ importers/BelvedereImporter.py
@@ -0,0 +1,25 @@
+from importers.GenericImporter import GenericImporter
+
+class BelvedereImporter(GenericImporter):
+    def __init__(self, db):
+        super().__init__(db)
+
+    def execute_row(self, row):
+        super().execute_row(row)
+        label, cd_number, upc, composer, title, artist, _, medium, year, cost, *_ = row
+
+        distributor = "Naxos"
+
+        distributor_id = self.get_distributor_id(distributor)
+        label_id = self.get_label_id(label, distributor_id)
+        medium_id = self.get_medium_id(medium)
+        price = self.get_sales_price(cost)
+
+        if self.check_upc(upc):
+            self.update_row(title, upc, medium_id, cd_number, composer, artist, year, label_id, distributor_id, cost,
+                      price)
+            print("update")
+        else:
+            self.add_row(title, upc, medium_id, cd_number, composer, artist, year, label_id, distributor_id, cost,
+                      price, blurb=blurb)
+            print("add")
