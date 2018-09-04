c4cf1669cf341cebc5cb9bc5e95ca8f53af146e6
Brendan Samek <xrendan@gmail.com>
2018-09-04 11:45:05 -0600

    Add Belvedere to Naxos

--- importers/BelvedereImporter.py
+++ importers/BelvedereImporter.py
@@ -4,9 +4,34 @@ class BelvedereImporter(GenericImporter):
     def __init__(self, db):
         super().__init__(db)
 
+    def get_row(self, row):
+        label, cd_number, upc, composer, title, artist, _, medium, year, cost, *_ = row
+        row_dict = dict()
+        row_dict["year"] = year
+        row_dict["label"] = label
+        row_dict["cd_number"] = cd_number
+        row_dict["upc"] = upc
+        row_dict["composer"] = composer
+        row_dict["title"] = title
+        row_dict["artist"] = artist
+        row_dict["medium"] = medium
+        row_dict["cost"] = cost
+
+        return row_dict
+
+
     def execute_row(self, row):
         super().execute_row(row)
-        label, cd_number, upc, composer, title, artist, _, medium, year, cost, *_ = row
+        row_dict = self.get_row(row)
+        year = row_dict["year"]
+        label = row_dict["label"]
+        cd_number = row_dict["cd_number"]
+        upc = row_dict["upc"]
+        composer = row_dict["composer"]
+        title = row_dict["title"]
+        artist = row_dict["artist"]
+        medium = row_dict["medium"]
+        cost = row_dict["cost"]
 
         distributor = "Naxos"
 
@@ -16,10 +41,12 @@ class BelvedereImporter(GenericImporter):
         price = self.get_sales_price(cost)
 
         if self.check_upc(upc):
-            self.update_row(title, upc, medium_id, cd_number, composer, artist, year, label_id, distributor_id, cost,
-                      price)
-            print("update")
+            # self.update_row(title, upc, medium_id, cd_number, composer, artist, year, label_id, distributor_id, cost,
+            #           price)
+            # print("update")
+            pass
         else:
             self.add_row(title, upc, medium_id, cd_number, composer, artist, year, label_id, distributor_id, cost,
-                      price, blurb=blurb)
+                         price)
+            # self.update_price_code(upc, price_code)
             print("add")
